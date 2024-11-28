701fd175fc26815f91972838d33240d7793da243
s.radyukov <s.radyukov@tinkoff.ru>
2024-11-28 15:36:40 +0300

    1.25.1 dev portfolio domain

--- example/src/main/java/ru/tinkoff/piapi/example/domain/portfolio/Portfolio.java
+++ example/src/main/java/ru/tinkoff/piapi/example/domain/portfolio/Portfolio.java
@@ -1,11 +1,13 @@
 package ru.tinkoff.piapi.example.domain.portfolio;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.collection.Map;
+import io.vavr.control.Either;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import ru.tinkoff.piapi.core.models.Quantity;
-import ru.tinkoff.piapi.example.domain.InstrumentId;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -16,74 +18,142 @@ import java.util.function.Function;
 public class Portfolio {
 
   final Map<PositionId, Position> positions;
-  final List<PendingOperation> pendingOperations;
-  final List<OperationEvent> events;
-
 
   public Portfolio withPositionQuantity(PositionId positionId, Quantity quantity) {
-    return new Portfolio(positions.put(positionId, new Position(positionId, BlockedAmount.of(quantity))), pendingOperations, this.events);
+    return new Portfolio(positions.put(positionId, new Position(positionId, BlockedAmount.of(quantity))));
   }
 
-  public Portfolio block(PositionId positionId, Quantity quantity) {
-    var computed = positions.computeIfPresent(positionId, (id, position) -> new Position(id, position.amount.block(quantity)
-      .getOrElseThrow(Function.identity())));
-    return new Portfolio(computed._2, pendingOperations, this.events);
+  public Either<NoBalanceAvailable, Tuple2<Portfolio, BlockedAmountEvent>> block(PositionId positionId, Quantity quantity) {
+    var event = new BlockedAmountEvent(positionId, quantity);
+    return applyEvent(event)
+      .map(portfolio -> Tuple.of(portfolio, event));
   }
 
-  public Portfolio unblock(PositionId positionId, Quantity quantity) {
-    var computed = positions.computeIfPresent(positionId, (id, position) -> new Position(id, position.amount.unblock(quantity)
-      .getOrElseThrow(Function.identity())));
-    return new Portfolio(computed._2, pendingOperations, this.events);
+  public Either<NoBalanceAvailable, Tuple2<Portfolio, UnblockedAmountEvent>> unblock(PositionId positionId, Quantity quantity) {
+    var event = new UnblockedAmountEvent(positionId, quantity);
+    return applyEvent(event)
+      .map(portfolio -> Tuple.of(portfolio, event));
   }
 
-  public Portfolio addQuantity(PositionId positionId, Quantity quantity) {
-    var computed = positions.computeIfPresent(positionId, (id, position) -> new Position(id, position.amount.add(quantity)));
-    return new Portfolio(computed._2, pendingOperations, this.events);
+  public Either<NoBalanceAvailable, Tuple2<Portfolio, PositionAmountAdded>> addQuantity(PositionId positionId, Quantity quantity) {
+    var event = new PositionAmountAdded(positionId, quantity);
+    return applyEvent(event)
+      .map(portfolio -> Tuple.of(portfolio, event));
   }
 
-  public Portfolio blockedWithdraw(PositionId positionId, Quantity quantity) {
-    var computed = positions.computeIfPresent(positionId, (id, position) -> new Position(id, position.amount.withdrawBlocked(quantity)
-      .getOrElseThrow(Function.identity())));
-    return new Portfolio(computed._2, pendingOperations, this.events);
+  public Either<NoBalanceAvailable, Tuple2<Portfolio, PositionAmountWithdrawnEvent>> blockedWithdraw(PositionId positionId, Quantity quantity) {
+    var event = new PositionAmountWithdrawnEvent(positionId, quantity);
+    return applyEvent(event)
+      .map(portfolio -> Tuple.of(portfolio, event));
   }
 
+  public Either<NoBalanceAvailable, Portfolio> applyEvent(OperationEvent event) {
+    switch (event.type) {
+      case POSITION_AMOUNT_ADDED:
+        var computed = positions
+          .computeIfAbsent(event.positionId, id -> new Position(id, BlockedAmount.zero()))
+          ._2
+          .computeIfPresent(
+            event.positionId,
+            (id, position) -> position.mapAmount(amount -> amount.add(event.quantity)));
+        return Either.right(new Portfolio(computed._2));
+      case POSITION_AMOUNT_WITHDRAWN:
+        return positions.get(event.positionId)
+          .map(position -> position.mapAmountEither(
+            amount -> amount.withdrawBlocked(event.quantity)
+          ))
+          .map(positionEither -> positionEither
+            .map(position -> positions.put(event.positionId, position))
+            .map(Portfolio::new)
+          )
+          .getOrElse(() -> Either.left(new NoBalanceAvailable("Position not found")));
+      case BLOCKED_AMOUNT_CHANGED:
+        return positions.get(event.positionId)
+          .map(position -> position.mapAmountEither(
+            amount -> amount.block(event.quantity)
+          ))
+          .map(positionEither -> positionEither
+            .map(position -> positions.put(event.positionId, position))
+            .map(Portfolio::new)
+          )
+          .getOrElse(() -> Either.left(new NoBalanceAvailable("Position not found")));
+      case UNBLOCKED_AMOUNT_CHANGED:
+        return positions.get(event.positionId)
+          .map(position -> position.mapAmountEither(
+            amount -> amount.unblock(event.quantity)
+          ))
+          .map(positionEither -> positionEither
+            .map(position -> positions.put(event.positionId, position))
+            .map(Portfolio::new)
+          )
+          .getOrElse(() -> Either.left(new NoBalanceAvailable("Position not found")));
+      default:
+        return Either.left(new NoBalanceAvailable("unknown type of event"));
+    }
+  }
 
   @RequiredArgsConstructor
   @Data
   public static class Position {
     final PositionId positionId;
     final BlockedAmount amount;
+
+    Position mapAmount(Function<BlockedAmount, BlockedAmount> f) {
+      return new Position(positionId, f.apply(amount));
+    }
+
+    Either<NoBalanceAvailable, Position> mapAmountEither(Function<BlockedAmount, Either<NoBalanceAvailable, BlockedAmount>> f) {
+      return f.apply(amount).map(nextAmount -> this.mapAmount(__ -> nextAmount));
+    }
   }
+
+
+  public static class BlockedAmountEvent extends OperationEvent {
+    public BlockedAmountEvent(PositionId positionId, Quantity quantity) {
+      super(positionId, OperationEventType.BLOCKED_AMOUNT_CHANGED, quantity);
+    }
+  }
+
+  public static class UnblockedAmountEvent extends OperationEvent {
+    public UnblockedAmountEvent(PositionId positionId, Quantity quantity) {
+      super(positionId, OperationEventType.UNBLOCKED_AMOUNT_CHANGED, quantity);
+    }
+  }
+
+  public static class PositionAmountWithdrawnEvent extends OperationEvent {
+    public PositionAmountWithdrawnEvent(PositionId positionId, Quantity quantity) {
+      super(positionId, OperationEventType.POSITION_AMOUNT_WITHDRAWN, quantity);
+    }
+  }
+
+  public static class PositionAmountAdded extends OperationEvent {
+    public PositionAmountAdded(PositionId positionId, Quantity quantity) {
+      super(positionId, OperationEventType.POSITION_AMOUNT_ADDED, quantity);
+    }
+  }
+
 
   @RequiredArgsConstructor
   @Data
-  public static class PendingOperation {
-    private final UUID id;
-    private final Position position;
-    private final OperationDirection direction;
-    private final Quantity quantity;
-    private final Quantity price;
-  }
-
-
-  public enum OperationDirection {
-    BUY, SELL
-  }
-
-
-  @RequiredArgsConstructor
-  @Data
-  public static class OperationEvent {
+  public abstract static class OperationEvent {
     final UUID eventId;
     final Instant eventTime;
     final PositionId positionId;
     final OperationEventType type;
     final Quantity quantity;
-    final Position position;
+
+    public OperationEvent(PositionId positionId, OperationEventType type, Quantity quantity) {
+      this(UUID.randomUUID(), Instant.now(), positionId, type, quantity);
+    }
+
+
   }
 
   public enum OperationEventType {
-    BUY, SELL, TAX, OTHER,
+    BLOCKED_AMOUNT_CHANGED,
+    UNBLOCKED_AMOUNT_CHANGED,
+    POSITION_AMOUNT_ADDED,
+    POSITION_AMOUNT_WITHDRAWN
   }
 
 }
